ac762b14a85e340e72d2bb95b0bb6079034f6077
dharmmaradiya <dharmmaradiya1906@gmail.com>
2026-03-22 13:34:19 +0530

    Add files via upload

---
+++ Profile.jsx
@@ -0,0 +1,567 @@
+import React from "react";
+
+export default function Profile() {
+    return(
+        <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+   
+    
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#Home">
+            Home
+          </a>
+          
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#About">
+            About
+          </a>
+          
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#Skill">
+           Skill
+          </a>
+          
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#Resume">
+            Resume
+          </a>
+          
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#Profile">
+            Profile
+          </a>
+          
+        </li>
+         <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#contact">
+           Contect
+          </a>
+          
+        </li>
+       
+      
+      
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+
+<div className="container" id="Home">
+    <div className="row mt-5 wow animate__animated animate__backInUp">
+  <div className="col-sm-6 mb-3 mb-sm-0 ">
+    <div className="card">
+      <div className="card-body mt-5">
+       <h1>Hello, I’m <br></br>Dharm Maradiya</h1>
+        <p className="card-text">
+          I'm a Full stack Web Devlopment and Developer based in London,<br></br> England. I strives to build immersive and beautiful web applications <br></br> through carefully crafted code and user-centric design.
+        </p>
+        <a  className="btn btn-secondary">
+        Know About Me
+        </a>
+      </div>
+    </div>
+  </div>
+  <div className="col-sm-6">
+    <div className="card">
+      <div className="card-body">
+      <div className="card" style={{ width: "24rem"  }} >
+  
+  <div className="card-body">
+    
+    <p className="card-text">
+      <img src="https://media-del3-1.cdn.whatsapp.net/v/t61.24694-24/620206819_1988891548647125_6868987404501113635_n.jpg?ccb=11-4&oh=01_Q5Aa4AEnXHYMTiKBcgJjXjMVmvPV5O8Mj5X8C595iRj5Og9UkA&oe=69CBAC37&_nc_sid=5e03e0&_nc_cat=109" className="card-img-top" alt="..." />
+    </p>
+   
+  </div>
+</div>
+
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
+
+<div className="container  wow animate__animated animate__backInUp" id='About'>
+    <div className="card mt-5 ">
+  <h5 className="card-header text-center">About</h5>
+  <div className="card-body">
+    
+    <p className="card-text">
+     Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et <br></br> nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.
+    </p>
+   
+  </div>
+</div>
+
+    <div className="card mb-3 mt-5 " style={{ maxWidth: 1400 }}>
+  <div className="row g-0">
+    <div className="col-md-4 ">
+      <img src="https://media-bom2-3.cdn.whatsapp.net/v/t61.24694-24/620206819_1988891548647125_6868987404501113635_n.jpg?ccb=11-4&oh=01_Q5Aa4AEouqYPMcSGjkjauZmPhKRi3PIuq6V1f3xz4V9kTo_I5w&oe=69CB3BB7&_nc_sid=5e03e0&_nc_cat=109" className="img-fluid rounded-start" alt="..." />
+    </div>
+    <div className="col-md-8">
+      <div className="card-body">
+        <h5 className="card-title">Full Stack Web And Devlopment</h5>
+        <p className="card-text">
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore <br></br> magna aliqua.
+        </p>
+       <div className="row wow animate__animated animate__backInRight">
+  <div className="col-sm-6 mb-3 mb-sm-0 ">
+    <div className="card">
+      <div className="card-body">
+       
+        <p className="card-text ">
+       
+        <p>Birthday:19 June 2008</p>
+        <p>Website:www.dharm1906@gmail.com</p>
+        <p>Phone:+7984250733</p>
+        <p>City:India</p>
+        
+        </p>
+       
+      </div>
+    </div>
+  </div>
+  <div className="col-sm-6">
+    <div className="card">
+      <div className="card-body">
+       
+        <p className="card-text">
+         <p>Age:17</p>
+         <p>Degree:Bechalor of computer Application</p>
+         <p>Email:dharm@1906gmail.com</p>
+         <p>Freelance:Available</p>
+        </p>
+        
+      </div>
+    </div>
+  </div>
+</div>
+<p>Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis. Cupiditate ut dicta maxime officiis quidem quia. Sed <br></br> et consectetur qui quia repellendus itaque neque.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
+<div className="container wow animate__animated animate__backInUp" id="Skill">
+    <div className="card mt-5">
+  <h5 className="card-header text-center ">Skill</h5>
+  <div className="card-body">
+   
+    <p className="card-text">
+     Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit
+    </p>
+  <div className="row wow animate__animated animate__backInRight">
+  <div className="col-sm-6 mb-3 mb-sm-0">
+    <div className="card">
+      <div className="card-body">
+     <div
+  className="progress"
+  role="progressbar"
+  aria-label="Example with label"
+  aria-valuenow={25}
+  aria-valuemin={0}
+  aria-valuemax={100}>
+  <div className="progress-bar" style={{ width: "100%" }}>
+    HTMl
+  </div>
+  
+</div>
+  <div
+  className="progress mt-5"
+  role="progressbar"
+  aria-label="Example with label"
+  aria-valuenow={25}
+  aria-valuemin={0}
+  aria-valuemax={100}>
+  <div className="progress-bar" style={{ width: "90%" }}>
+    CSS
+  </div>
+  
+</div>
+  <div
+  className="progress mt-5"
+  role="progressbar"
+  aria-label="Example with label"
+  aria-valuenow={25}
+  aria-valuemin={0}
+  aria-valuemax={100}>
+  <div className="progress-bar" style={{ width: "75%" }}>
+    JAVASCRIPT
+  </div>
+  
+</div>
+      </div>
+    </div>
+  </div>
+  <div className="col-sm-6 mb-3 mb-sm-0">
+  <div className="card">
+    <div className="card-body">
+     <div
+  className="progress "
+  role="progressbar"
+  aria-label="Example with label"
+  aria-valuenow={25}
+  aria-valuemin={0}
+  aria-valuemax={100}>
+  <div className="progress-bar" style={{ width: "80%" }}>
+    PHp
+  </div>
+  
+</div>
+     
+     <div
+  className="progress mt-5"
+  role="progressbar"
+  aria-label="Example with label"
+  aria-valuenow={25}
+  aria-valuemin={0}
+  aria-valuemax={100}>
+  <div className="progress-bar" style={{ width: "80%" }}>
+    SQL
+  </div>
+  
+</div><div
+  className="progress mt-5"
+  role="progressbar"
+  aria-label="Example with label"
+  aria-valuenow={25}
+  aria-valuemin={0}
+  aria-valuemax={100}>
+  <div className="progress-bar" style={{ width: "90%" }}>
+    DBMS
+  </div>
+  
+</div>
+      
+    </div>
+  </div>
+</div>
+
+</div>
+
+  </div>
+</div>
+
+</div>
+<div className="container wow animate__animated animate__backInUp" id="Resume">
+    <div className="card mt-5 ">
+  <div className="card-header text-center">Resume</div>
+  <div className="card-body">
+   
+    <p className="card-text">
+      Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et <br></br> nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.
+    </p>
+   <div className="row wow animate__animated animate__backInRight">
+  <div className="col-sm-6 mb-3 mb-sm-0">
+    <div className="card">
+      <div className="card-body">
+        <h6 className="card-title">Sumary</h6> <br></br>
+        <p className="card-text">
+         Brandon Johnson <br></br>
+          Innovative and deadline-driven Graphic Designer with 3+ years of experience <br></br> designing and developing user-centered digital/print marketing material from <br></br> initial concept to final, polished deliverable.
+ <br></br> 
+ <li>Portland par 127,Orlando, FL</li> <br></br>
+ <li>(123) 456-7891</li> <br></br>
+ <li>alice.barkley@example.com</li> <br></br>
+    <br></br>   <h6> Education</h6> <br></br>2015 - 2016<br></br>
+Rochester Institute of Technology, Rochester, NY <br></br>
+
+Qui deserunt veniam. Et sed aliquam labore tempore sed quisquam iusto autem <br></br> sit. Ea vero voluptatum qui ut dignissimos deleniti nerada porti sand markend
+<br></br> <br></br>  <h6>Bachelor of Fine Arts & Graphic Design</h6> <br></br> 
+Rochester Institute of Technology, Rochester, NY <br></br>
+2010 - 2014 <br></br>
+Quia nobis sequi est occaecati aut. Repudiandae et iusto quae reiciendis et <br></br> quis Eius vel ratione eius unde vitae rerum voluptates asperiores voluptatem <br></br> Earum molestiae consequatur neque etlon sader mart dila
+        </p>
+       
+      </div>
+    </div>
+  </div>
+  <div className="col-sm-6">
+    <div className="card">
+      <div className="card-body">
+        
+        <p className="card-text">
+   <h6> Professional Experience </h6>      <br></br>
+Senior graphic design specialist <br></br>
+2019 - Present <br></br>
+Experion, New York, NY <br></br>
+ 
+<li>Lead in the design, development, and implementation of the graphic, layout, and production communication materials</li> <br></br>
+ <li>Delegate tasks to the 7 members of the design team and provide counsel on all aspects of the project.</li> <br></br>
+<li> Supervise the assessment of all graphic materials in order to ensure quality and accuracy of the design </li> <br></br>
+ <li>Oversee the efficient use of production project budgets ranging from $2,000 - $25,000 </li><br></br>
+ <h5>Graphic design specialist </h5>
+2017 - 2018 <br></br>
+Stepping Stone Advertising, New York, NY <br></br>
+<li>Developed numerous marketing programs (logos, brochures,infographics, presentations, and advertisements).</li>
+<li>Managed up to 5 projects or tasks at a given time while under pressure</li>
+<li>Recommended and consulted with clients on the most appropriate graphic design</li>
+<li>Created 4+ design presentations and proposals a month for clients and account managers</li>
+        </p>
+       
+      </div>
+    </div>
+  </div>
+</div>
+
+  </div>
+</div>
+
+</div>
+<div className="container wow animate__animated animate__backInUp">
+    <div className="card mt-5" id="Profile">
+  <div className="card-header text-center">Profile</div>
+  <div className="card-body">
+<p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et <br></br> nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+  
+  <div className="row row-cols-1 row-cols-md-3 g-4 mt-3 wow animate__animated animate__backInUp">
+  <div className="col">
+    <div className="card h-100">
+      <img src="https://themewagon.github.io/iPortfolio/assets/img/portfolio/app-1.jpg" className="card-img-top" alt="..." />
+      <p className="d-none">hello</p>
+      
+    </div>
+  </div>
+  <div className="col">
+    <div className="card h-100">
+      <img src="https://themewagon.github.io/iPortfolio/assets/img/portfolio/product-1.jpg" className="card-img-top" alt="..." />
+      
+    </div>
+  </div>
+  <div className="col">
+    <div className="card h-100">
+      <img src="https://themewagon.github.io/iPortfolio/assets/img/portfolio/branding-1.jpg" className="card-img-top" alt="..." />
+      
+    </div>
+  </div>
+</div>
+<div className="row row-cols-1 row-cols-md-3 g-4 mt-3 wow animate__animated animate__backInUp">
+  <div className="col">
+    <div className="card h-100">
+      <img src="https://themewagon.github.io/iPortfolio/assets/img/portfolio/books-1.jpg" className="card-img-top" alt="..." />
+   
+    </div>
+  </div>
+  <div className="col">
+    <div className="card h-100">
+      <img src="	https://themewagon.github.io/iPortfolio/assets/img/portfolio/app-2.jpg" className="card-img-top" alt="..." />
+     
+    </div>
+  </div>
+  <div className="col">
+    <div className="card h-100">
+      <img src="https://themewagon.github.io/iPortfolio/assets/img/portfolio/product-2.jpg" className="card-img-top" alt="..." />
+      
+    </div>
+  </div>
+</div>
+<div className="row row-cols-1 row-cols-md-3 g-4 mt-3 wow animate__animated animate__backInUp">
+  <div className="col">
+    <div className="card h-100">
+      <img src="https://themewagon.github.io/iPortfolio/assets/img/portfolio/branding-2.jpg" className="card-img-top" alt="..." />
+    
+    </div>
+  </div>
+  <div className="col">
+    <div className="card h-100">
+      <img src="https://themewagon.github.io/iPortfolio/assets/img/portfolio/books-2.jpg" className="card-img-top" alt="..." />
+    
+    </div>
+  </div>
+  <div className="col">
+    <div className="card h-100">
+      <img src="https://themewagon.github.io/iPortfolio/assets/img/portfolio/app-3.jpg" className="card-img-top" alt="..." />
+     
+    </div>
+  </div>
+</div>
+
+<div className="row row-cols-1 row-cols-md-3 g-4 mt-3 wow animate__animated animate__backInUp">
+  <div className="col">
+    <div className="card h-100">
+      <img src="https://themewagon.github.io/iPortfolio/assets/img/portfolio/product-3.jpg" className="card-img-top" alt="..." />
+     
+    </div>
+  </div>
+  <div className="col">
+    <div className="card h-100">
+      <img src="https://themewagon.github.io/iPortfolio/assets/img/portfolio/branding-3.jpg" className="card-img-top" alt="..." />
+      
+    </div>
+  </div>
+  <div className="col">
+    <div className="card h-100">
+      <img src="https://themewagon.github.io/iPortfolio/assets/img/portfolio/books-3.jpg" className="card-img-top" alt="..." />
+     
+    </div>
+  </div>
+</div>
+
+  </div>
+</div>
+
+</div>
+  <div className="container mt-5  wow animate__animated animate__backInUp" id='contact'>
+<div className="card text-center">
+ <div className="card-header">Contact
+    <p className="card-text"> </p>
+   Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit
+                      </div>
+                </div>
+
+                <div className="container px-4 text-center">
+  <div className="row gx-5">
+    <div className="col">
+      <div className="p-3">
+        <div className="container py-5">
+  <form>
+    <div className="row">
+      
+      <div className="col-md-6 mb-4">
+        <input
+          type="text"
+          className="form-control custom-input"
+          placeholder="Firstname"
+        />
+      </div>
+      
+      <div className="col-md-6 mb-4">
+        <input
+          type="text"
+          className="form-control custom-input"
+          placeholder="Lastname"
+        />
+      </div>
+    </div>
+    
+    <div className="mb-4">
+      <input
+        type="email"
+        className="form-control custom-input"
+        placeholder="Email address"
+      />
+    </div>
+  
+    <div className="mb-4">
+      <textarea
+        className="form-control custom-input"
+        rows={5}
+        placeholder="Write your message..."
+        defaultValue={""}
+      />
+    </div>
+    
+    <button type="submit" className="btn btn-dark px-4 py-2">
+      Send Message
+    </button>
+  </form>
+</div>
+
+      </div>
+    </div>
+    <div className="col">
+      <div className="p-3">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d29373.35603589901!2d72.65976320000001!3d23.035904!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1773936580214!5m2!1sen!2sin"
+  width={600}
+  height={450}
+  style={{ border: 0 }}
+  allowFullScreen=""
+  loading="lazy"
+  referrerPolicy="no-referrer-when-downgrade"/>
+      </div>
+    </div>
+  </div>
+</div> 
+                </div>
+
+<div className="container">
+  <footer className="bg-light text-dark pt-5 pb-3 mt-5 wow animate__animated animate__backInUp">
+  <div className="container">
+    <div className="row">
+     
+      <div className="col-md-3">
+        <h5 className="fw-bold">Home</h5>
+        <ul className="list-unstyled">
+          <li>Untree.co</li>
+          <li>About us</li>
+          <li>Projects</li>
+          <li>Services</li>
+          <li>Terms</li>
+          <li>Privacy</li>
+        </ul>
+      </div>
+      
+      <div className="col-md-3">
+        <h5 className="fw-bold">Projects</h5>
+        <ul className="list-unstyled">
+          <li>HTML5</li>
+          <li>CSS3</li>
+          <li>Untree.co</li>
+          <li>Free Templates</li>
+          <li>WordPress Themes</li>
+        </ul>
+      </div>
+    
+      <div className="col-md-3">
+        <h5 className="fw-bold">Services</h5>
+        <ul className="list-unstyled">
+          <li>Untree.co</li>
+          <li>jQuery</li>
+          <li>Bootstrap</li>
+          <li>Freebies</li>
+        </ul>
+      </div>
+      
+      <div className="col-md-3">
+        <h5 className="fw-bold">Contact</h5>
+        <p>43 Raymouth Rd. Baltemoer, London 3910</p>
+        <p>+1(123)-456-7890</p>
+        <p>+1(123)-456-7890</p>
+        <p>info@mydomain.com</p>
+        <h6 className="fw-bold mt-3">Connect</h6>
+        <div className="d-flex gap-2">
+          <i className="bi bi-instagram" />
+          <i className="bi bi-twitter" />
+          <i className="bi bi-facebook" />
+          <i className="bi bi-linkedin" />
+          <i className="bi bi-pinterest" />
+        </div>
+      </div>
+    </div>
+   
+    <div className="text-center mt-4 border-top pt-3">
+      <p className="mb-0">
+        Copyright ©2026. All Rights Reserved. – Designed with love by Untree.co
+        Distributed By ThemeWagon
+      </p>
+    </div>
+  </div>
+</footer>
+
+</div>
+
+        </>
+    )
+}
