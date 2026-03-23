4bb6de57fcbc50760b737708b2b2bd823150bf92
dharmmaradiya <dharmmaradiya1906@gmail.com>
2026-03-23 10:50:31 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Todo from './compa/Todo';
+import  Todo2 from './compa/Todo2';
+function App() {
+  return (
+    <div className="App">
+     {/* <abc/> */}
+     {/* <Todo/>  */}
+      <Todo2/>
+    </div>
+  );
+}
+
+export default App;
